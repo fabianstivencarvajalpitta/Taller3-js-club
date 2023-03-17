11b8158395f7179b324c32f4e4f8d738859544a6
fabianstivencarvajalpitta <105111898+fabianstivencarvajalpitta@users.noreply.github.com>
2023-03-17 02:56:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const nombre = prompt ('ingresa tu nombre: ')
 const ingreso = prompt('ingresa tu edad');
 const miPromesa = new Promise((resolve, reject) => {
-  setTimeout(() => {
+
   if(ingreso >= 18 ){
-    resolve(`su nombre es: ${nombre},  su edad es: ${ingreso},  puedes ingresar al establecimiento `);
+    resolve(`su edad es: ${ingreso}, puedes ingresar al establecimiento `);
 
   }else if(ingreso <= 17){
-    resolve('no puede ingresar ya que eres menor de edad o su documento no es valido')
-}
-  }, 100);
+    reject(`tu edad es:${ingreso} no puede ingresar ya que eres menor de edad`)
+  }else if (ingreso != ingreso.isNan){}
+   reject(`Edad no validad`)
+
 });
 miPromesa.then((valor) => {
   console.log(valor)
@@ -16,5 +16,3 @@ miPromesa.then((valor) => {
   console.log(err)
 
 });
-
-
