4ab86b89e0e1580c00908625c1f9aff15faf2721
fabianstivencarvajalpitta <105111898+fabianstivencarvajalpitta@users.noreply.github.com>
2023-03-17 02:30:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-let promesaCumplida = Boolean(Math.round(Math.random()));
-console.log(promesaCumplida)
-if (promesaCumplida == true){
-}else promesaCumplida = true; 
-
-
-
-let ingreso = prompt();
-
+const nombre = prompt ('ingresa tu nombre: ')
+const ingreso = prompt('ingresa tu edad');
 const miPromesa = new Promise((resolve, reject) => {
   setTimeout(() => {
   if(ingreso >= 18 ){
-    resolve('puede ingresar');
+    resolve(`su nombre es: ${nombre},  su edad es: ${ingreso},  puedes ingresar al establecimiento `);
 
-  }if(ingreso <= 17){
-    resolve('no puede ingresar ya que es menor de edad o su documento no es valido')
-}else{
-    reject('promesa rechazada...');
-  }
-  }, 00);
+  }else if(ingreso <= 17){
+    resolve('no puede ingresar ya que eres menor de edad o su documento no es valido')
+}
+  }, 100);
 });
 miPromesa.then((valor) => {
   console.log(valor)
@@ -25,4 +16,5 @@ miPromesa.then((valor) => {
   console.log(err)
 
 });
+
 
